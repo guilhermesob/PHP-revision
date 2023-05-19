17e71ecc516825c5d207e811c08dd0dd0d4b9f9c
guilhermesob <101477908+guilhermesob@users.noreply.github.com>
2023-05-18 23:32:57 -0300

    Add files via upload

---
+++ comment.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title>Comment in PHP</title>
+</head>
+<body>
+    <?php
+    echo "How are you";
+    // single line comment
+    # single line comment
+    /* 
+    askjasldf
+    asdjfjasldf
+    ajsdjlfkjl
+    */
+    
+    ?>
+</body>
+</html>
---
+++ termination.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title>PHP Termination</title>
+</head>
+<body>
+    <?php
+    echo "Hello World";
+    echo (32+3423)
+    ?>
+</body>
+</html>
