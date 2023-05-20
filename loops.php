81797ea5a3b6ab95d9b6951bac2d99169d8cc551
guilhermesob <101477908+guilhermesob@users.noreply.github.com>
2023-05-19 23:39:37 -0300

    Add files via upload

---
+++ loops.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loops in PHP</title>
+</head>
+<body>
+   <?php
+        // for($i = 0; $i<=10; $i++){
+        //     // echo ("PHP"."<br>");
+        //     echo ($i);
+        // }
+
+        // $x = 0;
+        // while($x<=10){
+        //     // echo ($x);
+        //     echo "How are you";
+        //     echo "I am fine....";
+        //     $x++;
+        // }
+
+        $x = 10;
+        do{
+            echo ($x);
+            $x--;
+        }while($x>=1);
+
+   ?>
+</body>
+</html>
