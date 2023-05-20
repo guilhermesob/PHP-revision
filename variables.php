b1df10a7db4dd5799c736dd2c3defbc7d2e4f4b6
guilhermesob <101477908+guilhermesob@users.noreply.github.com>
2023-05-19 23:26:00 -0300

    Add files via upload

---
+++ variables.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Varibales in PHP</title>
+</head>
+<body>
+  <?php
+    $val = 232;
+    $name = "Faisal";
+    echo $name;
+    echo $val;
+  
+  ?>
+</body>
+</html>
