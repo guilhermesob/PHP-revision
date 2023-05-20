19f0a54bcd14393d2120e028fba6f3861ebb951a
guilhermesob <101477908+guilhermesob@users.noreply.github.com>
2023-05-19 23:25:30 -0300

    Add files via upload

---
+++ data_types.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Types in PHP</title>
+</head>
+<body>
+   <?php
+   $str = "Faisal zamir";
+   echo $str;
+   
+   $val = 32342;
+   $val1 = 2434.34;
+   $colors = array("gray","pink","yellow");
+   echo "<br>";
+   echo $colors[0];
+   $n = null;
+   echo $n;
+   
+   
+   
+   ?>
+
+</body>
+</html>
